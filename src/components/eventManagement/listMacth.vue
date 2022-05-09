c9dbda8c8c8cdf84ac2ba17b6d083fb0f4508b21
zhouyuding <3487379059@qq.com>
2022-05-09 22:33:47 +0800

    v 0.2.2

--- src/components/eventManagement/listMacth.vue
+++ src/components/eventManagement/listMacth.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="backColor">
+    <macth-seatch v-show="seatchShow" @macthSeatch="costPlannedAmountChange" />
     <div class="firstColor">
       <div>
-        <el-button type="danger" plain>批量删除</el-button>
+        <el-button type="success">添加</el-button>
+      </div>
+      <div style="padding-left: 20px">
+        <el-button type="danger" plain @click="macthDels">批量删除</el-button>
       </div>
       <div class="contentRight">
-        <el-button type="info" plain ref="btn1">查询</el-button>
+        <el-button type="info" plain ref="btn1" @click="showCont($event)"
+          >查询</el-button
+        >
       </div>
     </div>
     <div class="twons">
@@ -19,6 +25,8 @@
         row-id="id"
         :row-style="tableRowStyle"
         :header-row-style="tableStyle"
+        @checkbox-change="checkboxChangeEvent"
+        @checkbox-all="checkboxChangeEvent"
       >
         <vxe-column
           align="center"
@@ -26,13 +34,7 @@
           width="50"
           class="linker"
         ></vxe-column>
-        <vxe-column
-          type="seq"
-          title="序号"
-          width="60"
-          align="center"
-        ></vxe-column>
-        <vxe-column width="80" align="center">
+        <vxe-column width="50" align="center">
           <template v-slot="scoped">
             <el-image
               :src="
@@ -51,8 +53,13 @@
         </vxe-column>
         <vxe-column
           field="title"
-          title="赛事主题"
-          width="150"
+          title="赛事主题" 
+          align="center"
+        ></vxe-column>
+        <vxe-column
+          field="category"
+          title="征集类别" 
+          width="100"
           align="center"
         ></vxe-column>
         <vxe-column
@@ -63,6 +70,7 @@
         <vxe-column
           field="description"
           title="赛事简介"
+          width="100"
           align="center"
         ></vxe-column>
         <vxe-column
@@ -80,7 +88,7 @@
                   green: scoped.row.status === 1,
                   yellow: scoped.row.status == 0,
                   red: scoped.row.status === 2,
-                  blue:scoped.row.status === 3
+                  blue: scoped.row.status === 3,
                 }"
               >
                 {{ filterStatus(scoped.row.status) }}
@@ -88,42 +96,107 @@
             </div>
           </template>
         </vxe-column>
-
+        <vxe-column field="is_open" title="is_open" align="center" width="70">
+          <template v-slot="scoped">
+            <el-switch
+              v-model="scoped.row.is_open"
+              active-color="#13ce66"
+              @change="is_openChaged(scoped.row)"
+              :active-value="1"
+              :inactive-value="0"
+            ></el-switch>
+          </template>
+        </vxe-column>
         <vxe-column
           field="create_time"
           title="创建时间"
           align="center"
+          width="145"
           show-overflow="title"
         ></vxe-column>
         <vxe-column
           field="sign_time"
           title="报名时间"
           align="center"
+          width="145"
           show-overflow="title"
         ></vxe-column>
         <vxe-column
           field="exh_time"
           title="展示时间"
           align="center"
+          width="145"
           show-overflow="title"
         ></vxe-column>
-        <vxe-column field="browse" title="浏览量" align="center"></vxe-column>
-        <vxe-column title="操作" align="center"></vxe-column>
+        <vxe-column
+          field="browse"
+          title="浏览量"
+          align="center"
+          width="75"
+        ></vxe-column>
+        <vxe-column title="操作" align="center">
+          <template v-slot="scoped">
+            <div class="postDyex">
+              <div class="postDyexer">
+                <el-button type="success" @click="detailsValue(scoped.row)"
+                  >详情</el-button
+                >
+              </div>
+              <div class="postDyexer">
+                <el-button type="primary" @click="showEditAddmodify(scoped.row)"
+                  >修改</el-button
+                >
+              </div>
+              <div class="postDyexer">
+                <el-button type="danger" @click="oneCompany(scoped.row)"
+                  >删除</el-button
+                >
+              </div>
+            </div>
+          </template>
+        </vxe-column>
       </vxe-table>
     </div>
   </div>
 </template>
 
 <script>
+import macthSeatch from "./macthSeatch/macthSeatch.vue";
 import { postD } from "../../api/index.js";
 import { styleModify, styleModifytwo } from "../../assets/js/modifyStyle.js";
 export default {
+  provide() {
+    return {
+      MacthValue: this.MacthValue,
+    };
+  },
+  components: {
+    macthSeatch,
+  },
   data() {
     return {
+      seatchShow: false,
       allAlign: null,
       tableData: [],
       url: {
         listMacthInterface: "match/listMacth",
+        adminSelectDelMatchInterface: "Match/adminSelectDelMatch",
+        delMatchInterface: "Match/delMatch",
+      },
+      listMacth: {
+        id: "",
+        is_open: "",
+      },
+      // 批量删除
+      ids: [],
+      //选中时将对象保存到arrs中
+      arrs: [],
+      macthDelsValues: {
+        id: "",
+      },
+      // 单个删除
+      oneCompanyvalue: {
+        id: "",
       },
     };
   },
@@ -147,17 +220,122 @@ export default {
         return "结束";
       }
     },
+    // 开关
+    is_openChaged(data) {
+      this.listMacth.is_open = data.is_open;
+      this.listMacth.id = data.id;
+      postD(this.url.listMacthInterface, this.listMacth).then((res) => {
+        if (res.code == "200") {
+          this.$message.success("状态修改成功");
+        } else if (res.code == "-200") {
+          this.$message.error("参数错误，或暂无数据");
+        } else if (res.code == "-201") {
+          this.$message.error("未登陆");
+        } else if (res.code == "-203") {
+          this.$message.error("对不起，你没有此操作权限");
+        } else {
+          this.$message.error("注册失败，账号已存在");
+        }
+      });
+    },
+    // 批量删除
+    checkboxChangeEvent(data) {
+      this.arrs = data.records;
+    },
+    async macthDels() {
+      const macthDelsValue = await this.$confirm(
+        "此操作将永久删除管理, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).catch((err) => err);
+      if (macthDelsValue !== "confirm") {
+        return this.$message.info("取消删除");
+      }
+      if (selectDelValue === "confirm") {
+        this.arrs.forEach((v) => {
+          this.ids.push(v.id);
+        });
+        this.macthDelsValues.id = this.ids.toString();
+        postD(this.url.adminSelectDelMatchInterface, this.macthDelsValues).then(
+          (res) => {
+            if (res.code == "200") {
+              this.$message.success("状态修改成功");
+              this.MacthValue();
+            } else if (res.code == "-200") {
+              this.$message.error("参数错误，或暂无数据");
+            } else if (res.code == "-201") {
+              this.$message.error("未登陆");
+            } else if (res.code == "-203") {
+              this.$message.error("对不起，你没有此操作权限");
+            } else {
+              this.$message.error("注册失败，账号已存在");
+            }
+          }
+        );
+      }
+    },
+    // 单个删除
+    async oneCompany(data) {
+      const oneCompanyList = await this.$confirm(
+        "此操作将永久删除, 是否继续?",
+        "提示",
+        {
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
+        }
+      ).catch((err) => err);
+      if (oneCompanyList !== "confirm") {
+        return this.$message.info("取消删除");
+      }
+      if (oneCompanyList === "confirm") {
+        this.oneCompanyvalue.id = data.id;
+        postD(this.url.delMatchInterface, this.oneCompanyvalue).then((res) => {
+          if (res.code == "200") {
+            this.$message.success("状态修改成功");
+            this.MacthValue();
+          } else if (res.code == "-200") {
+            this.$message.error("参数错误，或暂无数据");
+          } else if (res.code == "-201") {
+            this.$message.error("未登陆");
+          } else if (res.code == "-203") {
+            this.$message.error("对不起，你没有此操作权限");
+          } else {
+            this.$message.error("注册失败，账号已存在");
+          }
+        });
+      }
+    },
     tableRowStyle() {
       styleModify();
     },
     tableStyle() {
       styleModifytwo();
     },
+    showCont() {
+      this.seatchShow = !this.seatchShow;
+      this.$refs.btn1.$el.innerText;
+    },
+    async costPlannedAmountChange(param1) {
+      this.tableData = param1;
+    },
   },
 };
 </script>
 <style lang="less" scoped>
 .contentRight {
-  padding-left: 90%;
+  padding-left: 86.5%;
+}
+.postDyex {
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+.postDyexer {
+  padding: 0 5px;
 }
 </style>
---
+++ src/components/eventManagement/macthSeatch/macthSeatch.vue
@@ -0,0 +1,136 @@
+<template>
+  <div>
+    <div class="contentLift">
+      <el-row :gutter="20">
+        <el-col :span="1"
+          ><div>
+            <div class="divText">关键词搜索:</div>
+          </div></el-col
+        >
+        <el-col :span="2"
+          ><div style="text-align: left">
+            <el-input
+              v-model="search.keyword"
+              placeholder="关键词搜索"
+            ></el-input></div
+        ></el-col>
+        <el-col :span="1"
+          ><div>
+            <div class="divText">时间范围:</div>
+          </div></el-col
+        >
+        <el-col :span="6"
+          ><div>
+            <div>
+              <el-date-picker
+                v-model="vipGetTime"
+                type="datetimerange"
+                range-separator="至"
+                start-placeholder="开始日期"
+                end-placeholder="结束日期"
+                @change="getTime"
+              >
+              </el-date-picker>
+            </div></div
+        ></el-col>
+        <el-col :span="1"
+          ><div>
+            <div class="divText">是否开启:</div>
+          </div></el-col
+        >
+        <el-col :span="3"
+          ><div>
+            <el-select v-model="search.is_open" placeholder="请选择">
+              <el-option
+                v-for="item in is_openOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select></div
+        ></el-col>
+
+        <el-col :span="3"
+          ><div>
+            <el-button type="success" @click="submitSearch">搜索</el-button>
+            <el-button type="info" @click="submitReset">重置</el-button>
+          </div></el-col
+        >
+        <el-col :span="1"
+          ><div>
+            <el-button @click="Refresh">刷新</el-button>
+          </div></el-col
+        >
+      </el-row>
+    </div>
+  </div>
+</template>
+<script>
+import { postD } from "../../../api/index.js";
+import {timestampToTime} from "../../../assets/js/time.js"
+export default {
+  inject: ["MacthValue"],
+  data() {
+    return {
+      url: {
+        listMacthInterface: "match/listMacth",
+      },
+      search: {
+        keyword: null,
+        sign_time:null,
+        exh_time:null,
+        is_open:null
+      },
+      vipGetTime:[],
+      is_openOptions:[
+          {
+          value: "1",
+          label: "开启",
+        },
+        {
+          value: "0",
+          label: "关闭",
+        },
+      ]
+    };
+  },
+  methods: {
+    Refresh() {
+      this.MacthValue();
+    },
+    submitReset() {
+      this.search.keyword = "";
+      this.search.sign_time = "";
+      this.search.exh_time = "";
+      this.search.is_open = "";
+      this.vipGetTime=""
+      
+      },
+      getTime(date) {
+      this.vipGetTime = date;
+      this.search.sign_time = timestampToTime(this.vipGetTime[0]/1000);
+      this.search.exh_time = timestampToTime(this.vipGetTime[1]/1000);
+    },
+      submitSearch() {
+      postD(this.url.listMacthInterface,this.search).then((res) => {
+        this.tableDatas = res.list;
+        this.$emit("macthSeatch", this.tableDatas);
+      });
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.contentLift {
+  text-align: left;
+  padding: 20px;
+  .divText {
+    width: 65px;
+    display: inline-block;
+    font-size: 9px;
+    line-height: 36px;
+  }
+}
+</style>
